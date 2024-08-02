3706d3d0e4d2fe57e19a5efae88073c98246ea53
staticBytess <160567397+staticBytess@users.noreply.github.com>
2024-08-02 00:45:09 -0700

    Body follows head. Goal cant be placed on player

--- source.cpp
+++ source.cpp
@@ -8,55 +8,81 @@
 
 using namespace std;
 
+/*Fix snake length*/
+
 void printGrid(const int user, const int goal, const float cellSize, sf::RenderWindow& window, sf::Font& font);
 void movePlayer(vector<int>& mapGrid, int& user, sf::RenderWindow& window, sf::Event& event, bool& refresh);
 void initVector(vector<int>& mapGrid);
-void placeGoal(const int user, int& goal);
+void placeGoal(const int user, int& goal, const vector<int> player);
+void continueMoving(vector<int> player);
 
 int const dimension = 9;
 int const vecSize = dimension * dimension;
 int const center = dimension * (dimension / 2) + dimension / 2;
+bool playerIsHere(const int cellNum, const vector<int> player);
 vector<int> mapGrid; // set size to dim*dim, and set each index equal to 0
+vector<int> playerWasHere;
 
 int main() {
-    sf::RenderWindow window(sf::VideoMode(800, 800), "Snake Game");
+    sf::RenderWindow window(sf::VideoMode(800, 800), "David's Snake");
+    sf::Clock clock;
+    sf::Time elapsed1 = clock.getElapsedTime();
+    std::cout << elapsed1.asSeconds() << std::endl;
+    clock.restart();
 
     // Load a font
     sf::Font font;
-    if (!font.loadFromFile("arial.ttf")) {
-        // Handle error
-        return -1;
-    }
+    font.loadFromFile("arial.ttf");
 
     // Size of each cell (assuming a square window)
     float cellSize = window.getSize().x / dimension;
     srand(time(NULL));
     int user = center;
+    int lastPos = user;
     int goal;
+    int level = 1;
     bool refresh;
     initVector(mapGrid);
-    placeGoal(user, goal);
+    placeGoal(user, goal, playerWasHere);
 
     while (window.isOpen()) {
         sf::Event event;
         refresh = false;
-
-        //checks events from user
-        movePlayer(mapGrid, user, window, event, refresh);
-
-
         window.clear();
 
+        //checks events from user
+        lastPos = user;
+        movePlayer(mapGrid, user, window, event, refresh);
+        
         printGrid(user, goal, cellSize, window, font);
 
         //refreshes the user's position
         if (refresh) {
             user = center;
-            placeGoal(user, goal);
+            placeGoal(user, goal, playerWasHere);
         }
         else if(user == goal) {
-            placeGoal(user, goal);
+            playerWasHere.insert(playerWasHere.begin(), user);
+            placeGoal(user, goal, playerWasHere);
+            level++;
+
         }
+        else {
+
+            if (lastPos != user) {
+                playerWasHere.insert(playerWasHere.begin(), user);
+                cout << "\nplayer size:" << playerWasHere.size() << "\n";
+                for (int i = 0; i < playerWasHere.size(); i++) {
+                    cout << playerWasHere[i];
+                }
+            }
+
+            
+            if (playerWasHere.size() > level) {
+                playerWasHere.pop_back();
+            }
+        }
+
 
         window.display();
     }
@@ -65,19 +91,25 @@ int main() {
 }
 
 void printGrid(const int user, const int goal, const float cellSize, sf::RenderWindow& window, sf::Font& font) {
-    // Draw the grid
     string cellNum;
+    int cellNumber;
+
     for (int row = 0; row < dimension; ++row) {
+
         for (int col = 0; col < dimension; ++col) {
+            cellNumber = mapGrid[row * dimension + col];
 
             sf::RectangleShape cell(sf::Vector2f(cellSize, cellSize));
             cell.setPosition(col * cellSize, row * cellSize);
 
             // Set cell color based on its value in mapGrid
-            if (mapGrid[row * dimension + col] == user) {
+            if (cellNumber == user) {
+                cell.setFillColor(sf::Color::Black);  // User cell
+            }
+            else if (playerIsHere(cellNumber, playerWasHere)) {
                 cell.setFillColor(sf::Color::Red);  // User cell
             }
-            else if (mapGrid[row * dimension + col] == goal) {
+            else if (cellNumber == goal) {
                 cell.setFillColor(sf::Color::Yellow);  // User cell
             }
             else {
@@ -139,17 +171,27 @@ void initVector(vector<int>& mapGrid) {
     }
 }
 
-void placeGoal(const int user, int& goal) {
-    int max = vecSize;
+void placeGoal(const int user, int& goal, const vector<int> player) {
+    int max = vecSize-1;
     int min = 0;
     int randNum;
     
     randNum = rand() % (max - min + 1) + min;
     
-    while (randNum == user /*|| alreadyVisited(randNum, playerWasHere)*/) {
+    while (randNum == user || playerIsHere(randNum, player)) {
         randNum = rand() % (max - min + 1) + min;
         //cout << randNum << " ";
     }
     
     goal = randNum;
+}
+
+bool playerIsHere(const int cellNum, const vector<int> player) {
+    if (!player.empty()){ 
+        for (int i = 0; i < player.size(); i++) {
+            if (player[i] == cellNum)
+                return true;
+        }
+    }
+    return false;
 }
