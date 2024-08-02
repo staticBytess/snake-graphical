4ac3c0c3c6d60db52fb06c6a71028e0076667db0
staticBytess <160567397+staticBytess@users.noreply.github.com>
2024-08-02 02:19:36 -0700

    added single move function, delay, + auto movement

--- source.cpp
+++ source.cpp
@@ -11,24 +11,24 @@ using namespace std;
 /*Fix snake length*/
 
 void printGrid(const int user, const int goal, const float cellSize, sf::RenderWindow& window, sf::Font& font);
-void movePlayer(vector<int>& mapGrid, int& user, sf::RenderWindow& window, sf::Event& event, bool& refresh);
+char getNextMove(const char prevChar, bool& refresh, sf::RenderWindow& window, sf::Event& event, bool& active);
+void movePlayer(int& user, const char input, const float delay, sf::Clock& clock);
 void initVector(vector<int>& mapGrid);
 void placeGoal(const int user, int& goal, const vector<int> player);
-void continueMoving(vector<int> player);
+void updatePlayerVector(const int user, const int lastPos, const int level, vector<int>& player);
+void resetVector(vector<int>& resetMe);
 
 int const dimension = 9;
 int const vecSize = dimension * dimension;
 int const center = dimension * (dimension / 2) + dimension / 2;
 bool playerIsHere(const int cellNum, const vector<int> player);
 vector<int> mapGrid; // set size to dim*dim, and set each index equal to 0
-vector<int> playerWasHere;
+vector<int> snakeBody;
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(800, 800), "David's Snake");
     sf::Clock clock;
-    sf::Time elapsed1 = clock.getElapsedTime();
-    std::cout << elapsed1.asSeconds() << std::endl;
-    clock.restart();
+    
 
     // Load a font
     sf::Font font;
@@ -41,46 +41,51 @@ int main() {
     int lastPos = user;
     int goal;
     int level = 1;
+    const float userDelayInSeconds = 0.15f;
+    const float autoDelayInSeconds = 0.2f;
+
     bool refresh;
+    bool active;
+    char input = 0;
     initVector(mapGrid);
-    placeGoal(user, goal, playerWasHere);
+    placeGoal(user, goal, snakeBody);
 
     while (window.isOpen()) {
         sf::Event event;
         refresh = false;
+        active = false;
         window.clear();
 
         //checks events from user
         lastPos = user;
-        movePlayer(mapGrid, user, window, event, refresh);
+        input = getNextMove(input, refresh, window, event, active);
+
+        if (active) {
+            movePlayer(user, input, userDelayInSeconds, clock);
+        }
+        else {
+            movePlayer(user, input, autoDelayInSeconds, clock);
+        }
         
         printGrid(user, goal, cellSize, window, font);
 
         //refreshes the user's position
         if (refresh) {
             user = center;
-            placeGoal(user, goal, playerWasHere);
+            resetVector(snakeBody);
+            placeGoal(user, goal, snakeBody);
+
+
         }
         else if(user == goal) {
-            playerWasHere.insert(playerWasHere.begin(), user);
-            placeGoal(user, goal, playerWasHere);
+            snakeBody.insert(snakeBody.begin(), user);
+            placeGoal(user, goal, snakeBody);
             level++;
 
         }
         else {
-
-            if (lastPos != user) {
-                playerWasHere.insert(playerWasHere.begin(), user);
-                cout << "\nplayer size:" << playerWasHere.size() << "\n";
-                for (int i = 0; i < playerWasHere.size(); i++) {
-                    cout << playerWasHere[i];
-                }
-            }
-
+            updatePlayerVector(user, lastPos, level, snakeBody);
             
-            if (playerWasHere.size() > level) {
-                playerWasHere.pop_back();
-            }
         }
 
 
@@ -106,7 +111,7 @@ void printGrid(const int user, const int goal, const float cellSize, sf::RenderW
             if (cellNumber == user) {
                 cell.setFillColor(sf::Color::Black);  // User cell
             }
-            else if (playerIsHere(cellNumber, playerWasHere)) {
+            else if (playerIsHere(cellNumber, snakeBody)) {
                 cell.setFillColor(sf::Color::Red);  // User cell
             }
             else if (cellNumber == goal) {
@@ -140,30 +145,40 @@ void printGrid(const int user, const int goal, const float cellSize, sf::RenderW
     }
 }
 
-
-void movePlayer(vector<int>& mapGrid, int& user, sf::RenderWindow& window, sf::Event& event, bool& refresh) {
-    while (window.pollEvent(event)) {
-        if (event.type == sf::Event::Closed)
-            window.close();
-        if (event.type == sf::Event::KeyPressed) {
-            if (event.key.scancode == sf::Keyboard::Scan::Escape) {
-                refresh = true;
-            }
-            else if (event.key.scancode == sf::Keyboard::Scan::W && validMove(user, user - dimension, dimension)) {
+void movePlayer(int& user, const char input, const float delay, sf::Clock& clock) {
+    
+    if (clock.getElapsedTime().asSeconds() > delay) {
+        switch (input) {
+        case 'w':
+            if (validMove(user, user - dimension, dimension)) {
                 user -= dimension;
+                clock.restart();
             }
-            else if (event.key.scancode == sf::Keyboard::Scan::S && validMove(user, user + dimension, dimension)) {
+            break;
+        case 's':
+            if (validMove(user, user + dimension, dimension)) {
                 user += dimension;
+                clock.restart();
             }
-            else if (event.key.scancode == sf::Keyboard::Scan::D && validMove(user, user + 1, dimension)) {
-                user += 1;
-            }
-            else if (event.key.scancode == sf::Keyboard::Scan::A && validMove(user, user - 1, dimension)) {
+            break;
+        case 'a':
+            if (validMove(user, user - 1, dimension)) {
                 user -= 1;
+                clock.restart();
             }
+            break;
+        case 'd':
+            if (validMove(user, user + 1, dimension)) {
+                user += 1;
+                clock.restart();
+            }
+            break;
         }
     }
 }
+
+
+
 
 void initVector(vector<int>& mapGrid) {
     for (int i = 0; i < vecSize; i++) {
@@ -194,4 +209,55 @@ bool playerIsHere(const int cellNum, const vector<int> player) {
         }
     }
     return false;
+}
+
+
+void updatePlayerVector(const int user, const int lastPos, const int level, vector<int>& player) {
+    if (lastPos != user) {
+        player.insert(player.begin(), user);
+        cout << "\nplayer size:" << player.size() << "\n";
+        for (int i = 0; i < player.size(); i++) {
+            cout << player[i];
+        }
+    }
+
+
+    if (player.size() > level) {
+        player.pop_back();
+    }
+}
+
+char getNextMove(const char prevChar, bool& refresh, sf::RenderWindow& window, sf::Event& event, bool& active) {
+    while (window.pollEvent(event)) {
+        if (event.type == sf::Event::Closed)
+            window.close();
+        if (event.type == sf::Event::KeyPressed) {
+            if (event.key.scancode == sf::Keyboard::Scan::Escape) {
+                refresh = true;
+                return 'c';
+            }
+            else if (event.key.scancode == sf::Keyboard::Scan::W) {
+                active = true;
+                    return 'w';
+            }
+            else if (event.key.scancode == sf::Keyboard::Scan::S) {
+                active = true;
+                    return 's';
+            }
+            else if (event.key.scancode == sf::Keyboard::Scan::D) {
+                active = true;
+                    return 'd';
+            }
+            else if (event.key.scancode == sf::Keyboard::Scan::A) {
+                active = true;
+                return 'a';
+            }
+        }
+        
+    }
+    return prevChar;
+}
+void resetVector(vector<int>& resetMe) {
+    vector<int> temp;
+    resetMe = temp;
 }
